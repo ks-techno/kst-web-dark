48f98d619726bed15845543d622de877ca5f53ff
Mughees Ahmed <mughees.ahmed@ks-technologies.net>
2023-03-27 16:28:12 +0500

    Dark-theme-components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { LandingPageComponent } from './about-us/landing-page/landing-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutUsComponent,
+    LandingPageComponent
   ],
   imports: [
     BrowserModule,
