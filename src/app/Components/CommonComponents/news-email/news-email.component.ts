8b3bf7d25ad68fcfd79e459598035f8d0c6fd1f4
asrar ahmed <asrar.ahmed@ks-technologies.net>
2023-04-06 14:45:43 +0500

    api response

--- src/app/Components/CommonComponents/news-email/news-email.component.ts
+++ src/app/Components/CommonComponents/news-email/news-email.component.ts
@@ -11,7 +11,7 @@ import { environment } from 'src/enironment/enviornment';
 
 export class NewsEmailComponent {
   emailForm: FormGroup;
-  message!:string
+  message!: string
 
   constructor(private fb: FormBuilder, private http: HttpClient) {
     this.emailForm = this.fb.group({
@@ -19,29 +19,29 @@ export class NewsEmailComponent {
     });
   }
 
-  onSubmit() {
-    interface ApiResponse {
-      [key: string]: any;
-    }
-    const apiUrl = environment.baseUrl+"subscribe";
-    const headers = new HttpHeaders({
-      "Content-Type": "application/json",
-      "Access-Control-Allow-Origin": "*"
-    });
-    if (this.emailForm.valid) {
-      const email = this.emailForm.value.email;
-      this.http.post(apiUrl, { email:email })
-        .subscribe((response: ApiResponse) => {
-          console.log("response", response);
+onSubmit() {
+  interface ApiResponse {
+    [key: string]: any;
+  }
+  const apiUrl = environment.baseUrl + "subscribe";
+  const headers = new HttpHeaders({
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "*"
+  });
+  if (this.emailForm.valid) {
+    const email = this.emailForm.value.email;
+    this.http.post(apiUrl, { email: email })
+      .subscribe((response: ApiResponse) => {
+        console.log("response", response);
         if (response && response?.['message']) {
           this.message = response?.['message'];
         } else {
           this.message = "Unexpected response from server";
         }
         this.emailForm.reset();
-        }, error => {
-          this.message = "Subscription failed";
-        });
+      }, error => {
+        this.message = "Subscription failed";
+      });
     }
   }
 }
