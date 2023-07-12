643ab4aeb83694a789b1c218cf474743eaadfab1
asrar ahmed <asrar.ahmed@ks-technologies.net>
2023-07-12 15:50:14 +0500

    sticky header added

--- src/app/Components/Pages/jobs/jobs-grid/jobs-grid.component.ts
+++ src/app/Components/Pages/jobs/jobs-grid/jobs-grid.component.ts
@@ -6,5 +6,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./jobs-grid.component.css']
 })
 export class JobsGridComponent {
-
+  heading = 'KS-Technologies Jobs'
+  jobs = [
+    {
+      title: '',
+      subtitle: '',
+      text: ''
+    },
+    // {
+    //   title: 'Graphic Designer',
+    //   subtitle: 'Bahria Town, Lahore',
+    //   text: 'We are looking for a graphic designer to turn our projects into visually appealing products for our clients. Graphic Designer responsibilities include creating visual concepts, using computer software, and communicating ideas that inspire, inform, and captivate consumers.'
+    // },
+    // {
+    //   title: '',
+    //   subtitle: 'Bahria Town,Lahore',
+    //   text: '',
+    // },
+    // {
+    //   title: 'Social Media Marketer Intern',
+    //   subtitle: 'Bahria Town,Lahore',
+    //   text: 'We are looking for a UI/UX designer to turn our projects into easy-to-use products for our clients. UI/UX Designer responsibilities include gathering user requirements, designing graphic elements and building navigation components.',
+    // },
+    // {
+    //   title: 'Senior Web Developer',
+    //   subtitle: 'Bahria Town,Lahore',
+    //   text: 'We are looking for a UI/UX designer to turn our projects into easy-to-use products for our clients. UI/UX Designer responsibilities include gathering user requirements, designing graphic elements and building navigation components.',
+    // },
+    // {
+    //   title: 'Senior Front End/UI Developer',
+    //   subtitle: 'Bahria Town,Lahore',
+    //   text: 'We are looking for a UI/UX designer to turn our projects into easy-to-use products for our clients. UI/UX Designer responsibilities include gathering user requirements, designing graphic elements and building navigation components.',
+    // },
+  ];
 }
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,68 +9,110 @@ import { PrivacyPoliciyComponent } from './Components/Pages/privacy-policiy/priv
 import { ErrorComponent } from './Components/Pages/error/error.component';
 import { LocationStrategy, HashLocationStrategy } from '@angular/common';
 import { BlogComponent } from './Components/Pages/blog/blog.component';
+import { JobsComponent } from './Components/Pages/jobs/jobs.component';
 
 
+// const routes: Routes = [
+//   { path: '', redirectTo: 'home', pathMatch: 'full' },
+//   {
+//     component: HomeComponent,
+//     path: "home",
+//     data: {
+//       heading: 'KS-Technologies',
+//       subheading: 'A Software development company devoted to your project\'s success which accelerate your digital growth with our solutions.'
+//     }
+//   },
+//   {
+//     component: ContactComponent,
+//     path: "contact",
+//     data: {
+//       heading: 'Contact Us',
+//       subheading: 'Get in touch with our team'
+//     }
+//   },
+//   {
+//     component: PortfolioComponent,
+//     path: "portfolio",
+//     data: {
+//       heading: 'Portfolio',
+//       subheading: 'We are a team of expert developers, designers, and project managers who are passionate about delivering high-quality software solutions to our clients.'
+//     }
+//   },
+//   {
+//     component: AboutComponent,
+//     path: "about",
+//     data: {
+//       heading: 'About Us',
+//       subheading: 'KS-Technologies is a software development company. Our team of expert developers are skilled in React JS, AWS, Angular, WordPress, Shopify, Block chain, Machine Learning, PMO Department, Python with AI, Web3 Solidity and ROR, allowing us to develop software for a variety of platforms and industries. We are dedicated to provide top-quality software solutions to our clients, and we take pride in delivering software that meets and exceeds their expectations.'
+//     }
+//   },
+//   {
+//     component: OurServicesComponent,
+//     path: "our-service",
+//     data: {
+//       heading: 'Our Services',
+//       subheading: 'We are dedicated to provide top-quality software solutions to our clients, and we take pride in delivering software that meets and exceeds their expectations. We strive to stay on top of the latest trends and technologies to provide our clients with cutting-edge software solutions.'
+//     }
+//   },
+//   {
+//     component: BlogComponent,
+//     path: "blog",
+//     data: {
+//       heading: '',
+//       subheading: ''
+//     }
+//   },
+//   {
+//     component: PrivacyPoliciyComponent,
+//     path: "privacy-policy",
+//     data: {
+//       heading: 'Privacy Policy',
+//       subheading: ''
+//     }
+//   },
+//   { path: '**', component: ErrorComponent }
+// ];
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   {
     component: HomeComponent,
-    path: "home",
-    data: {
-      heading: 'KS-Technologies',
-      subheading: 'A Software development company devoted to your project\'s success which accelerate your digital growth with our solutions.'
-    }
-  },
-  {
-    component: ContactComponent,
-    path: "contact",
-    data: {
-      heading: 'Contact Us',
-      subheading: 'Get in touch with our team'
-    }
-  },
-  {
-    component: PortfolioComponent,
-    path: "portfolio",
-    data: {
-      heading: 'Portfolio',
-      subheading: 'We are a team of expert developers, designers, and project managers who are passionate about delivering high-quality software solutions to our clients.'
-    }
+    path: "home"
   },
   {
     component: AboutComponent,
-    path: "about",
-    data: {
-      heading: 'About Us',
-      subheading: 'KS-Technologies is a software development company. Our team of expert developers are skilled in React JS, AWS, Angular, WordPress, Shopify, Block chain, Machine Learning, PMO Department, Python with AI, Web3 Solidity and ROR, allowing us to develop software for a variety of platforms and industries. We are dedicated to provide top-quality software solutions to our clients, and we take pride in delivering software that meets and exceeds their expectations.'
-    }
+    path: 'about'
   },
   {
     component: OurServicesComponent,
-    path: "our-service",
-    data: {
-      heading: 'Our Services',
-      subheading: 'We are dedicated to provide top-quality software solutions to our clients, and we take pride in delivering software that meets and exceeds their expectations. We strive to stay on top of the latest trends and technologies to provide our clients with cutting-edge software solutions.'
-    }
+    path: "our-service"
   },
   {
     component: BlogComponent,
-    path: "blog",
-    data: {
-      heading: 'Blog & News',
-      subheading: 'We are dedicated to provide top-quality software solutions to our clients, and we take pride in delivering software that meets and exceeds their expectations. We strive to stay on top of the latest trends and technologies to provide our clients with cutting-edge software solutions.'
-    }
+    path: "blog"
+  },
+  {
+    component: PortfolioComponent,
+    path: "portfolio"
+  },
+  {
+    component: PortfolioComponent,
+    path: "portfolio"
   },
   {
     component: PrivacyPoliciyComponent,
-    path: "privacy-policy",
-    data: {
-      heading: 'Privacy Policy',
-      subheading: ''
-    }
+    path: "privacy-policy"
+  },
+  {
+    component: JobsComponent,
+    path: "jobs"
+  },
+  {
+    component: ContactComponent,
+    path: "contact"
   },
   { path: '**', component: ErrorComponent }
 ];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, {
